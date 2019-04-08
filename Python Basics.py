bedaceb040ac48e9d1481b7cd7f3a596843ad1fa
zz9521 <49339616+zz9521@users.noreply.github.com>
2019-04-08 14:04:22 +0100

    Update Python Basics.py

--- Python Basics.py
+++ Python Basics.py
@@ -1,3 +1,35 @@
 #input and output
 a=input()
 print(a)
+
+#Built-in Functions
+#https://docs.python.org/3/library/functions.html
+#https://www.jb51.net/article/60896.htm
+help(abs)
+
+#calculation
+abs(-1)
+max(1,2,3)
+min(1,2,3)
+len('apple')
+divmod(10,3) #return(10/3,10%3)
+pow(2,4)   #2**4
+pow(2,3,7) #2**3%4
+round(1.3)  #1
+
+#
+callable(abs)
+isinstance(2,str)
+
+#format
+int('123')
+int(12.34)
+float('12.34')
+str(1.23)
+str(100)
+bool(1)
+bool('')
+
+
+#define function
+
